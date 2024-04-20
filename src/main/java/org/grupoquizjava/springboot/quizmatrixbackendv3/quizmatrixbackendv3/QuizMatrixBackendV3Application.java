b22b26593147b285476df88c358e2e71b089ffb0
Mario Fernandez <marioteleco@outlook.com>
2024-04-20 19:19:53 +0200

    New Spring Boot Project created

---
+++ src/main/java/org/grupoquizjava/springboot/quizmatrixbackendv3/quizmatrixbackendv3/QuizMatrixBackendV3Application.java
@@ -0,0 +1,13 @@
+package org.grupoquizjava.springboot.quizmatrixbackendv3.quizmatrixbackendv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizMatrixBackendV3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizMatrixBackendV3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/grupoquizjava/springboot/quizmatrixbackendv3/quizmatrixbackendv3/QuizMatrixBackendV3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.grupoquizjava.springboot.quizmatrixbackendv3.quizmatrixbackendv3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizMatrixBackendV3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
